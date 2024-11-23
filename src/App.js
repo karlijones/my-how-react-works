1f66c90d7f41c0abe49ae5268d426ea7ab844dbb
karlijones <karlijones1984@gmail.com>
2024-11-23 11:23:42 -0600

    testing state update with handleTripleInc function

--- src/App.js
+++ src/App.js
@@ -72,6 +72,12 @@ function TabContent({ item }) {
     setLikes(likes + 1);
   }
 
+  function handleTripleInc() {
+    setLikes(likes + 1);
+    setLikes(likes + 1);
+    setLikes(likes + 1);
+  }
+
   function handleUndo() {
     setShowDetails(true)
     setLikes(0);
@@ -90,6 +96,7 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
+          <button onClick={handleTripleInc}>+++</button>
           <button>+++</button>
         </div>
       </div>
